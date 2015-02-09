7aa03fdbcb03ca2fca6121b08b1a8c645a24052a
fengmk2 <fengmk2@gmail.com>
2015-02-09 16:44:25 +0800

    fix: make more spec-compliant
    Fixes #1

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var copy = require('copy-to');
  * CORS middleware
  *
  * @param {Object} [options]
- *  - {String|Function(ctx)} origin `Access-Control-Allow-Origin`, default is '*'
+ *  - {String|Function(ctx)} origin `Access-Control-Allow-Origin`, default is request Origin header
  *  - {String|Array} allowMethods `Access-Control-Allow-Methods`, default is 'GET,HEAD,PUT,POST,DELETE'
  *  - {String|Array} exposeHeaders `Access-Control-Expose-Headers`
  *  - {String|Array} allowHeaders `Access-Control-Allow-Headers`
@@ -27,9 +27,6 @@ var copy = require('copy-to');
  */
 module.exports = function (options) {
   var defaults = {
-    origin: function (ctx) {
-      return ctx.get('origin') || '*';
-    },
     allowMethods: 'GET,HEAD,PUT,POST,DELETE'
   };
 
@@ -55,47 +52,70 @@ module.exports = function (options) {
   options.credentials = !!options.credentials;
 
   return function* cors(next) {
-    var origin = options.origin;
-    if (origin === false) {
+    // If the Origin header is not present terminate this set of steps. The request is outside the scope of this specification.
+    var requestOrigin = this.get('Origin');
+    if (!requestOrigin) {
       return yield* next;
     }
 
+    var origin;
     if (typeof options.origin === 'function') {
       origin = options.origin(this);
-    }
-
-    if (origin === false) {
-      return yield* next;
-    }
-
-    this.set('Access-Control-Allow-Origin', origin);
-
-    if (options.exposeHeaders) {
-      this.set('Access-Control-Expose-Headers', options.exposeHeaders);
-    }
-
-    if (options.maxAge) {
-      this.set('Access-Control-Max-Age', options.maxAge);
-    }
-
-    if (options.credentials === true) {
-      this.set('Access-Control-Allow-Credentials', 'true');
-    }
-
-    this.set('Access-Control-Allow-Methods', options.allowMethods);
-
-    var allowHeaders = options.allowHeaders;
-    if (!allowHeaders) {
-      allowHeaders = this.get('access-control-request-headers');
-    }
-    if (allowHeaders) {
-      this.set('Access-Control-Allow-Headers', allowHeaders);
-    }
-
-    if (this.method === 'OPTIONS') {
-      this.status = 204;
+      if (!origin) {
+        return yield* next;
+      }
     } else {
+      origin = options.origin || requestOrigin;
+    }
+
+    if (this.method !== 'OPTIONS') {
+      // Simple Cross-Origin Request, Actual Request, and Redirects
+
+      this.set('Access-Control-Allow-Origin', origin);
+
+      if (options.credentials === true) {
+        this.set('Access-Control-Allow-Credentials', 'true');
+      }
+
+      if (options.exposeHeaders) {
+        this.set('Access-Control-Expose-Headers', options.exposeHeaders);
+      }
+
       yield* next;
+    } else {
+      // Preflight Request
+
+      // If there is no Access-Control-Request-Method header or if parsing failed,
+      // do not set any additional headers and terminate this set of steps.
+      // The request is outside the scope of this specification.
+      if (!this.get('Access-Control-Request-Method')) {
+        // this not preflight request, ignore it
+        return yield* next;
+      }
+
+      this.set('Access-Control-Allow-Origin', origin);
+
+      if (options.credentials === true) {
+        this.set('Access-Control-Allow-Credentials', 'true');
+      }
+
+      if (options.maxAge) {
+        this.set('Access-Control-Max-Age', options.maxAge);
+      }
+
+      if (options.allowMethods) {
+        this.set('Access-Control-Allow-Methods', options.allowMethods);
+      }
+
+      var allowHeaders = options.allowHeaders;
+      if (!allowHeaders) {
+        allowHeaders = this.get('Access-Control-Request-Headers');
+      }
+      if (allowHeaders) {
+        this.set('Access-Control-Allow-Headers', allowHeaders);
+      }
+
+      this.status = 204;
     }
   };
 };
