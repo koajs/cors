c4a23d963fe98c2a6b5aa4064b040506b6b1d534
Yiyu He <dead_horse@qq.com>
2016-05-14 21:55:31 +0800

    fix: keepHeadersOnError won't affect OPTIONS request (#17)

--- test/cors.test.js
+++ test/cors.test.js
@@ -342,6 +342,22 @@ describe('cors.test.js', function() {
       .expect(500, done);
     });
 
+    it('should not affect OPTIONS requests', function(done) {
+      const app = new Koa();
+      app.use(cors());
+      app.use(function(ctx) {
+        ctx.body = {foo: 'bar'};
+        throw new Error('Whoops!');
+      });
+
+      request(app.listen())
+      .options('/')
+      .set('Origin', 'http://koajs.com')
+      .set('Access-Control-Request-Method', 'PUT')
+      .expect('Access-Control-Allow-Origin', 'http://koajs.com')
+      .expect(204, done);
+    });
+
     it('should not keep unrelated headers', function(done) {
       const app = new Koa();
       app.use(cors());
